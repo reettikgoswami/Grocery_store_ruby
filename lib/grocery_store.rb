25ef0bc650ff8fc94147fe7d955179fbd5a08045
reettik97 <reettikgoswami1997@gmail.com>
2021-02-16 23:13:55 +0530

    Show grocery bill

--- lib/grocery_store.rb
+++ lib/grocery_store.rb
@@ -1,4 +1,15 @@
+require_relative "billing"
+
 class GroceryStore 
+  def initialize 
+    @grocery_orders = []
+    @PRODUCTS = {
+      milk: 3.97,
+      bread: 2.17,
+      banana: 0.99,
+      apple: 0.89
+    } 
+  end
 
   def take_order 
     puts "Please enter all the items purchased separated by a comma"
@@ -6,10 +17,23 @@ class GroceryStore
 
     if (user_input.length > 0)
       grocery_items_array =  user_input.downcase.split(",").map { |item| item.gsub(/\s+/, "") } 
-      puts grocery_items_array
+      add_to_grocery_cart(grocery_items_array)
     else
       puts "No input provided." 
       take_order
+    end
+  end
+
+  def add_to_grocery_cart(ordered_items) 
+    @grocery_orders = ordered_items.select { |item|  !@PRODUCTS[item.to_sym].nil? }
+    print_bill
+  end
+
+  def print_bill 
+    if (@grocery_orders.length > 0)
+      Billing.new(@grocery_orders).receipt
+    else
+      puts "No order has been placed."
     end
   end
 
